9b8ae648d7123a0cc6db2bc4bbf964c0894ce6bb
Hara Keisuke <harakeisuke7@gmail.com>
2025-05-12 16:51:20 +0900

    修正３

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 console.log('🛠 next.config.ts → isProd=', isProd)
 
 const nextConfig: NextConfig = {
-  // output: 'export',
+  output: 'export',
   trailingSlash: true,
   // 開発時は空文字、本番時だけ /hitblow-next を付与
   basePath: isProd ? '/hitblow-next' : '',
