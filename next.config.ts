db2ac8eed2349f6c572ab5fc40c800b9c476a678
Hara Keisuke <harakeisuke7@gmail.com>
2025-05-12 12:25:14 +0900

    修正３

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,24 @@
+// next.config.ts
+import { NextConfig } from 'next'
+
+const isProd = process.env.NODE_ENV === 'production'
+console.log('🛠 next.config.ts → isProd=', isProd)
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/hitblow-next' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/hitblow-next/' : '',
+  // 開発時は空文字、本番時だけ /hitblow-next を付与
+  basePath: isProd ? '/hitblow-next' : '',
+  assetPrefix: isProd ? '/hitblow-next/' : '',
+  eslint: {
+    ignoreDuringBuilds: true,    // ビルド時の ESLint エラーを無視
+  },
   webpack(config, { isServer }) {
-    if (!isServer) config.resolve.fallback = { fs: false, path: false }
+    if (!isServer) {
+      config.resolve.fallback = { fs: false, path: false }
+    }
     return config
   },
 }
+
 export default nextConfig
